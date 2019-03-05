e15fe0f403945c4251e48e22f87b19d93ed5a0a5
Kamil <paryzowy@gmail.com>
2019-03-05 01:35:19 +0100

    add font loader to production webpack

--- build-utils/webpack.production.js
+++ build-utils/webpack.production.js
@@ -32,7 +32,8 @@ module.exports = () => ({
           'postcss-loader',
           'sass-loader',
         ],
-      }
+      },
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
     ]
   },
 
