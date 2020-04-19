cf0679ded60664cfdb249e1b55e587ed002595ad
Kamil Zieliński <paryzowy@gmail.com>
2020-04-19 18:44:17 +0200

    add generate pdf button

--- src/elm/Page/RcBeam.elm
+++ src/elm/Page/RcBeam.elm
@@ -5,8 +5,8 @@ import Bootstrap.Grid.Col as Col
 import Bootstrap.Grid.Row as Row
 import Calculator.Beam
 import Calculator.Types as Types
-import Html exposing (Html, div, strong, text)
-import Html.Attributes exposing (class)
+import Html exposing (Html, a, div, strong, text)
+import Html.Attributes exposing (class, href, target)
 import Page.RcBeam.Partials.BeamDrawing as BeamDrawing
 import Page.RcBeam.Partials.Form as Form
 import Page.RcBeam.Partials.Results as Results
@@ -193,7 +193,8 @@ view model =
                 [ Grid.col [ Col.lg12, Col.xl9 ]
                     [ Grid.row [ Row.centerMd ]
                         [ Grid.col [ Col.middleXs, Col.lg12, Col.xl6 ]
-                            [ Form.render model.beam ]
+                            [ Form.render model.beam
+                            ]
                         , Grid.col [ Col.middleXs, Col.lg12, Col.xl6, Col.attrs [ class "drawing" ] ]
                             [ BeamDrawing.render model.beam model.reinforcement ]
                         ]
@@ -207,9 +208,83 @@ view model =
                         ]
                     , Grid.row [ Row.centerMd ]
                         (Tables.render top bottom)
+                    , Grid.row [ Row.centerMd ]
+                        [ Grid.col [ Col.xs12 ]
+                            [ a
+                                [ href <| generatePdfUrl <| Translator.withCalcs model.beam
+                                , target "_blank"
+                                , class "btn btn-secondary"
+                                ]
+                                [ text "Print results" ]
+                            ]
+                        ]
                     ]
                 , Grid.col [ Col.lg12, Col.xl3 ]
                     [ Grid.row [ Row.centerMd ] [ Results.template ] ]
                 ]
             ]
     }
+
+
+generatePdfUrl : StringedResultBeam -> String
+generatePdfUrl beamResults =
+    String.concat
+        [ baseUrlWithFunction
+        , "height="
+        , beamResults.height
+        , "&width="
+        , beamResults.width
+        , "&cover="
+        , beamResults.cover
+        , "&topCover="
+        , beamResults.topCover
+        , "&concreteClass="
+        , beamResults.concreteClass
+        , "&steelClass="
+        , beamResults.steelClass
+        , "&concreteFactor="
+        , beamResults.concreteFactor
+        , "&steelFactor="
+        , beamResults.steelFactor
+        , "&alphaCC="
+        , beamResults.alphaCC
+        , "&linkDiameter="
+        , beamResults.linkDiameter
+        , "&mainBarDiameter="
+        , beamResults.mainBarDiameter
+        , "&bendingMoment="
+        , beamResults.bendingMoment
+        , "&fcd="
+        , beamResults.fcd
+        , "&fctm="
+        , beamResults.fctm
+        , "&fyd="
+        , beamResults.fyd
+        , "&effectiveHeight="
+        , beamResults.effectiveHeight
+        , "&minReinforcement="
+        , beamResults.minReinforcement
+        , "&sC="
+        , beamResults.sC
+        , "&ksiEffective="
+        , beamResults.ksiEffective
+        , "&ksiEffectiveLim="
+        , beamResults.ksiEffectiveLim
+        , "&bottomReinforcementPrime="
+        , beamResults.bottomReinforcementPrime
+        , "&bendingMomentPrime="
+        , beamResults.bendingMomentPrime
+        , "&bendingMomentDelta="
+        , beamResults.bendingMomentDelta
+        , "&topReqReinforcement="
+        , beamResults.topReqReinforcement
+        , "&bottomReqReinforcement="
+        , beamResults.bottomReqReinforcement
+        , "&maximumReinforcement="
+        , beamResults.maximumReinforcement
+        ]
+
+
+baseUrlWithFunction : String
+baseUrlWithFunction =
+    "https://rc-calculator-backend.herokuapp.com/api/renderpdf?"
